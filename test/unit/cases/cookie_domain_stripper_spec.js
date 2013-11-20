bde6a53d139b329b273fd17ee497c54a6245976f
Jan Nanista <jan.nanista@gooddata.com>
2013-11-20 15:19:32 +0100

    BUGFIX: CL-2558 Cannot login to staging using grizzly dev mode - domain header is not translated

--- test/unit/cases/cookie_domain_stripper_spec.js
+++ test/unit/cases/cookie_domain_stripper_spec.js
@@ -8,7 +8,7 @@ describe('cookie_domain_stripper', function() {
     });
 
     it('should call original writeHead method', function () {
-        var res = { writeHead: function() {} };
+        var res = { writeHead: function() {}, getHeader: function() {} };
         var spy = spyOn(res, 'writeHead');
 
         this.object({}, res, function() {});
@@ -19,7 +19,7 @@ describe('cookie_domain_stripper', function() {
     });
 
     it('should strip domain from `set-cookie` header', function () {
-        var res = { writeHead: function() {} };
+        var res = { writeHead: function() {}, setHeader: function() {} };
         var spy = spyOn(res, 'writeHead');
 
         var header = 'GDCAuthSST=x8qtuB7HjE1pJ4xx; path=/gdc/account; domain=secure.gooddata.com; secure; HttpOnly';
@@ -32,5 +32,19 @@ describe('cookie_domain_stripper', function() {
         res.writeHead(200, headers);
 
         expect(spy).toHaveBeenCalledWith(200, modifiedHeaders);
+    });
+
+    it('should strip domain from `set-cookie` header when headers are passed in response object', function () {
+        var res = { writeHead: function() {}, getHeader: function() { return [header]; }, setHeader: function() {} };
+        var spy = spyOn(res, 'setHeader');
+
+        var header = 'GDCAuthSST=x8qtuB7HjE1pJ4xx; path=/gdc/account; domain=secure.gooddata.com; secure; HttpOnly';
+        var modifiedHeader = 'GDCAuthSST=x8qtuB7HjE1pJ4xx; path=/gdc/account; secure; HttpOnly';
+
+        this.object({}, res, function() {});
+
+        res.writeHead(200);
+
+        expect(spy).toHaveBeenCalledWith('set-cookie', [modifiedHeader]);
     });
 });
