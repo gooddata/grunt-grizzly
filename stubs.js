8fc21af90f55c204e22d833a5ad286c1fec46b50
Martin Cermak <martin.cermak@gooddata.com>
2016-11-23 20:59:10 +0100

    Tests

--- stubs.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (app) {
-    app.use('/stubtest/:arg?', function (req, res) {
-        res.status(200).send('Hello World, your arg: ' + req.params.arg);
-    });
-
-    return app;
-};
