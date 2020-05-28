cf1da9ea829e979d606950aec0eb14dccaba5a52
Vlastimil Kotas <vlastimil.kotas@gooddata.com>
2020-05-28 10:03:59 +0200

    FEATURE: FET-430 Upgrade dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,5 @@
 // Copyright (C) 2007-2017, GoodData(R) Corporation.
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
     // Load all required tasks
     grunt.loadTasks('tasks');
